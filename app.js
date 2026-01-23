7e17041a468b04d6c43865f5012e274e09e4926d
misaelgocu <misaelgomez00000@gmail.com>
2026-01-22 19:47:44 -0600

    config de body parser para apirest

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 app.get('/', (req, res) => {
   res.send(`
@@ -12,6 +17,22 @@ app.get('/', (req, res) => {
     `);
 });
 
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  res.send(`Mostrar información del usuario con ID: ${userId}`);
+});
+
+app.get('/search', (req, res) => {
+  const terms = req.query.termino || 'No especificado';
+  const category = req.query.categoria || 'Todas';
+
+  res.send(`
+      <h2>Resultados de Busqueda:</h2>
+      <p>Término: ${terms}</p>
+      <p>Categoría: ${category}</p>
+    `);
+});
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on http://localhost:${PORT}`);
+  console.log(`Servidor: http://localhost:${PORT}`);
 });
